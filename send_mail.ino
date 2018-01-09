4b18ee1e8794422829112ea28867059db1d37c18
theismunk <35096021+theismunk@users.noreply.github.com>
2018-01-09 15:18:26 +0100

    Add files via upload

---
+++ send_mail.ino
@@ -0,0 +1,112 @@
+#include <ESP8266WiFi.h>  // the ESP8266WiFi.h  lib
+const char* SSID = "DTU-Networks";
+const char* PASS = "Fotonik343";
+char server[] = "mail.smtp2go.com";
+ADC_MODE(ADC_VCC);
+
+WiFiClient client;
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+  Serial.println("");
+  Serial.println("");
+  Serial.print("Connecting");
+  Serial.println(SSID);
+  WiFi.begin(SSID, PASS);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi Connected");
+  Serial.print("IPess: ");
+  Serial.println(WiFi.localIP());
+  byte ret = sendEmail();
+}
+
+void loop()
+{
+}
+
+byte sendEmail()
+{
+  byte thisByte = 0;
+  byte respCode;
+
+  if (client.connect(server, 2525) == 1) {
+    Serial.println(F("connected"));
+  } else {
+    Serial.println(F("connection failed"));
+    return 0;
+  }
+  if (!eRcv()) return 0;
+
+  Serial.println(F("Sending EHLO"));
+  client.println("EHLO www.example.com");
+  if (!eRcv()) return 0;
+  Serial.println(F("Sending auth login"));
+  client.println("auth login");
+  if (!eRcv()) return 0;
+  Serial.println(F("Sending User"));
+  // Change to your base64, ASCII encoded user
+  client.println("dGhlaXNtdW5r"); // SMTP UserID
+  if (!eRcv()) return 0;
+  Serial.println(F("Sending Password"));
+  // change to your base64, ASCII encoded password
+  client.println("bWFQeFNVTXBhZTNt");//  SMTP Passw
+     if (!eRcv()) return 0;
+    Serial.println(F("Sending From"));   // change to your email address (sender)
+   client.println(F("MAIL From: theis_mj@hotmail.com"));// not important 
+   if (!eRcv()) return 0;   // change to recipient address
+    Serial.println(F("Sending To"));
+    client.println(F("RCPT To: theis_mj@hotmail.com"));
+    if (!eRcv()) return 0;
+    Serial.println(F("Sending DATA"));
+    client.println(F("DATA"));
+    if (!eRcv()) return 0;
+    Serial.println(F("Sending email"));   // change to recipient address
+   client.println(F("To: theis_mj@hotmail.com"));   // change to your address
+   client.println(F("From: theis_mj@hotmail.com"));
+ client.println(F("Subject: Status update\r\n"));
+    client.println(F("There has been some movement."));
+    client.println(F("."));
+    if (!eRcv()) return 0;
+    Serial.println(F("Sending QUIT"));
+    client.println(F("QUIT"));
+    if (!eRcv()) return 0;
+    client.stop();
+    Serial.println(F("disconnected"));
+    return 1;
+  }
+  byte eRcv()
+  {
+    byte respCode;
+    byte thisByte;
+    int loopCount = 0;
+    while (!client.available())
+  {
+      delay(1);
+      loopCount++;     // if nothing received for 10 seconds, timeout
+      if (loopCount > 10000) {
+      client.stop();
+      Serial.println(F("\r\nTimeout"));
+      return 0;
+    }
+  }
+
+  respCode = client.peek();
+  while (client.available())
+  {
+    thisByte = client.read();
+    Serial.write(thisByte);
+  }
+
+  if (respCode >= '4')
+  {
+    //  efail();
+    return 0;
+  }
+  return 1;
+}
---
+++ sketch_hm10.ino
@@ -0,0 +1,61 @@
+#include <AltSoftSerial.h>
+AltSoftSerial BTSerial;
+
+boolean DEBUG = true;
+
+#define LED_PIN 2
+#define rxPin 1
+#define txPin 0
+
+// SoftwareSerial mySerial(txPin, rxPin); // RX, TX  
+// Connect HM10      Arduino Uno
+//     Pin 1/TXD          Pin 7
+//     Pin 2/RXD          Pin 8
+
+void setup() {  
+  if (DEBUG) {
+    Serial.begin(9600);
+    Serial.println(__FILE__);
+    Serial.println(__DATE__);
+    Serial.println(" ");
+  }
+
+   //  open software serial connection to the Bluetooth module.
+  BTSerial.begin(9600); 
+  if (DEBUG)  {   Serial.println("BTSerial started at 9600");
+     }
+  
+  // If the baudrate of the HM-10 module has been updated,
+  // you may need to change 9600 by another value
+  // Once you have found the correct baudrate,
+  // you can update it using AT+BAUDx command 
+  // e.g. AT+BAUD0 for 9600 bauds
+  
+  pinMode(LED_PIN, OUTPUT);
+//  pinMode(rxPin, INPUT);
+//  pinMode(txPin, OUTPUT);
+
+  }
+
+
+void loop() {  
+  int c;
+
+  
+  if (BTSerial.available() > 0) {
+    c = BTSerial.read();  
+    Serial.println("Got input:");
+    if (c != 0)
+    {
+      // Non-zero input means "turn on LED".
+      Serial.println("  on");
+      digitalWrite(LED_PIN, HIGH);
+    }
+    else
+    {
+      // Input value zero means "turn off LED".
+      Serial.println("  off");
+      digitalWrite(LED_PIN, LOW);
+    }  
+  }
+}
